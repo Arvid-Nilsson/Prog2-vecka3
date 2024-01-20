029e29c2f57f9fcebb10f97a6a2aa59e06ebbf0a
Arvid Nilsson <arvnil27@gmail.com>
2024-01-20 15:17:56 +0100

    Färdig med uppgift 2

---
+++ Uppgift2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CircleCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.Write("Ange cirkelns radie i m: ");
+        double Radius = double.Parse(Console.ReadLine());
+        double Area = Math.Round(Math.PI * Math.Pow(Radius, 2),2);
+        Console.WriteLine($"Arean på en cirkel med radien {Radius} är {Area}");
+    }
+  }
+}
