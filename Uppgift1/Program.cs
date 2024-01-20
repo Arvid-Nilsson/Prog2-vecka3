a01be582e6ac6b49d7cc7554049bf3669c4d4037
Arvid Nilsson <arvnil27@gmail.com>
2024-01-20 15:02:46 +0100

    Uppgift 1 klart

--- Uppgift1/Program.cs
+++ Uppgift1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AllmänaÖvningar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double värde1 = 3.1415;
+      double värde2 = 2.7182;
+      double summa = värde1 * värde2;
+      Console.WriteLine(summa);
+      int lokaltHögstaVärde = Math.Max(5, 10);
+      Console.WriteLine(lokaltHögstaVärde);
+      double högstaVärde = Math.Max(lokaltHögstaVärde, summa);
+      Console.WriteLine(högstaVärde);
+    }
+  }
+}
